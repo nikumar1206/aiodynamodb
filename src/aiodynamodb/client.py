4957a09e7b65fed47183e2e6e6d052be0ad95664
Alberto Calderari <aberto.calderari@gmail.com>
2026-02-16 22:15:33 +0000

    add basic methods

--- src/aiodynamodb/client.py
+++ src/aiodynamodb/client.py
@@ -12,4 +12,14 @@ class DynamoDB(aioboto3.Session):
         async with self.resource('dynamodb') as c:
             yield c
 
-    
+    def scan[T: DynamoBaseModel](self, model: type[T]):
+        pass
+
+    def query[T: DynamoBaseModel](self, model: type[T]):
+        pass
+
+    def get[T: DynamoBaseModel](self, model: type[T]) -> T:
+        pass
+
+    def put[T: DynamoBaseModel](self, model: T):
+        pass
