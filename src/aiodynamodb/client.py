e2dcf93ea41fb5dc88bcb0733d258937a65ff357
Alberto Calderari <aberto.calderari@gmail.com>
2026-02-16 22:54:07 +0000

    add a couple more methods

--- src/aiodynamodb/client.py
+++ src/aiodynamodb/client.py
@@ -8,6 +8,13 @@ import aioboto3
 
 type Key = int | str
 
+
+class Page[T]:
+    next_key: str
+    items;
+    list[T]
+
+
 class DynamoDB(aioboto3.Session):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -18,17 +25,55 @@ class DynamoDB(aioboto3.Session):
         async with self.resource('dynamodb') as c:
             yield c
 
-    async def scan[T: DynamoBaseModel](self, table: str, model: type[T]):
+    async def scan[H: Key, R: Key, T: DynamoBaseModel](
+            self,
+            table: str,
+            model: type[T]
+    ) -> Page[T]:
         pass
 
-    async def query[H, R, T: DynamoBaseModel](self, table: str, hash_key: H, range_key: R | None, model: type[T]) -> T:
+    async def query[H: Key, R: Key, T: DynamoBaseModel](
+            self,
+            table: str,
+            hash_key: str,
+            hash_key_value: H,
+            model: type[T]
+    ) -> Page[T]:
         pass
 
-    async def get[H, R, T: DynamoBaseModel](self, table: str, hash_key: H, range_key: R | None, model: type[T]) -> T:
-        pass
+    async def get[H: Key, R: Key, T: DynamoBaseModel](
+            self,
+            table: str,
+            hash_key: str,
+            hash_key_value: H,
+            range_key: str | None,
+            range_key_value: R | None,
+            model: type[T]
+    ) -> T | None:
+        c: DynamoDBClient
+        key = {hash_key: SERIALIZER(hash_key_value)}
+        if range_key:
+            key[range_key] = SERIALIZER(range_key_value)
+        async with self.client() as c:
+            return await c.get_item(
+                TableName=table,
+                Key=key
+            )
 
-    async def put[T: DynamoBaseModel](self, table: str, model: T):
-        pass
+    async def put[H: Key, R: Key, T: DynamoBaseModel](
+            self,
+            table: str,
+            model: T
+    ) -> None:
+        c: DynamoDBClient
+        key = {hash_key: SERIALIZER(hash_key_value)}
+        if range_key:
+            key[range_key] = SERIALIZER(range_key_value)
+        async with self.client() as c:
+            await c.put_item(
+                TableName=table,
+                Item=model.model_dump()
+            )
 
     async def delete[H: Key, R: Key](
             self,
@@ -37,7 +82,7 @@ class DynamoDB(aioboto3.Session):
             hash_key_value: H,
             range_key: str | None,
             range_key_value: R | None
-    ):
+    ) -> None:
         c: DynamoDBClient
         key = {hash_key: SERIALIZER(hash_key_value)}
         if range_key:
